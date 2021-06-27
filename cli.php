9e69f7e5cc65b646330b9a0ba8b1df8f3e298892
Simon Eddy <simon@simoneddy.com.au>
2021-06-28 00:29:53 +1000

    add setup check to cli

--- cli.php
+++ cli.php
@@ -1,11 +1,16 @@
 <?php
 require 'vendor/autoload.php';
 
-$app = include_once 'bootstrap/app.php';
+if (!\Eddy\Framework\Setup\SetUp::isSetUp($dir = projectDir())) {
+    \Eddy\Framework\Setup\SetUp::run($dir);
+} else {
+    $app = include_once 'bootstrap/app.php';
 
-/**
- * @var \Symfony\Component\Console\Application
- */
-$cli = $app[\Symfony\Component\Console\Application::class];
+    /**
+     * @var \Symfony\Component\Console\Application
+     */
+    $cli = $app[\Symfony\Component\Console\Application::class];
+    
+    $cli->run();
+}
 
-$cli->run();
